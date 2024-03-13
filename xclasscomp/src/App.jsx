60291ac808e6088ed6d756d1e3569c4deeeeb07c
Akshaykumar <akshaykumarpatil33@gmail.com>
2024-03-13 14:11:44 +0530

    class compo counter  final commit

--- xclasscomp/src/App.jsx
+++ xclasscomp/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+  handleIncrement = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Counter App</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
